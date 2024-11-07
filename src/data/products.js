d72c67b1af227893da655410432b76d1985df3f9
ThuThao <thaoptqe170211@fpt.edu.vn>
2024-11-07 12:40:23 +0700

    123

--- src/data/products.js
+++ src/data/products.js
@@ -2,15 +2,15 @@ const products = [
   {
       id: 1,
       name: "Áo yếm nhỏ mùa hè dành cho nữ có đệm ngực Áo khoác ngoài không tay Áo ngắn",
-      description: "A stylish t-shirt made from high-quality materials, perfect for any occasion.",
-      price: 25.99,
+      description: "",
+      price: 500.377,
       originalPrice: 35.99,
-      sold: 150,
+      sold: 50,
       rating: 4.8,
       reviews: 124,
       imageUrl: require('../image/mau15.jpg'), // Đường dẫn đến ảnh sản phẩm
       colors: ["Red", "Blue", "Green"],
-      sizes: ["S", "M", "L", "XL"],
+      sizes: ["S", "M", "L",],
       brand: "FashionBrand",
       features: [
           "100% Premium Cotton",
@@ -21,14 +21,14 @@ const products = [
   },
   {
       id: 2,
-      name: 'Áo thun cổ tròn cotton',
-      description: 'High-quality cotton t-shirt for everyday wear',
-      price: 29.99,
+      name: 'Bộ váy quấn ren đơn giản cho nữ',
+      description: '',
+      price: 178.890,
       originalPrice: 39.99,
-      sold: 50,
+      sold: 150,
       rating: 4.8,
       reviews: 98,
-      imageUrl: require('../image/mau16.jpg'),
+      imageUrl: require('../image/mau3.jpg'),
       colors: ["Black", "White", "Gray"],
       sizes: ["S", "M", "L", "XL"],
       brand: "CottonCo",
@@ -41,16 +41,16 @@ const products = [
   },
   {
       id: 3,
-      name: 'Quần jean thời trang',
-      description: 'Classic denim jeans with a modern fit',
-      price: 49.99,
+      name: 'Set áo gile nữ 2 lớp, chân váy xếp ly Hanni chất vải cao cấp ',
+      description: '',
+      price: 180.010,
       originalPrice: 59.99,
-      sold: 30,
+      sold: 938,
       rating: 4.2,
       reviews: 45,
-      imageUrl: require('../image/mau10.jpg'),
-      colors: ["Blue", "Dark Blue", "Black"],
-      sizes: ["28", "30", "32", "34"],
+      imageUrl: require('../image/mau4.jpg'),
+      colors: ["Brown"],
+      sizes: ["S", "M", "L"],
       brand: "DenimBrand",
       features: [
           "Comfort stretch denim",
@@ -59,6 +59,127 @@ const products = [
           "Timeless design"
       ]
   },
+  {
+    id: 4,
+    name: 'Áo thun form rộng Paradox tay lỡ - Unisex ',
+    description: '',
+    price: 190.000,
+    originalPrice: 59.99,
+    sold: 990,
+    rating: 4.2,
+    reviews: 45,
+    imageUrl: require('../image/mau19.jpg'),
+    colors: ["Blue", "Dark Blue", "Black"],
+    sizes: ["S", "M", "L"],
+    brand: "DenimBrand",
+    features: [
+        "Comfort stretch denim",
+        "Classic 5-pocket styling",
+        "Machine washable",
+        "Timeless design"
+    ]
+},
+{
+    id: 5,
+    name: 'Váy xòe ngắn họa tiết kim cương chất liệu satin cao cấp ',
+    description: '',
+    price: 650.010,
+    originalPrice: 59.99,
+    sold: 20,
+    rating: 4.8,
+    reviews: 45,
+    imageUrl: require('../image/mau16.jpg'),
+    colors: ["White",],
+    sizes: ["S", "M", "L"],
+    brand: "DenimBrand",
+    features: [
+        "Comfort stretch denim",
+        "Classic 5-pocket styling",
+        "Machine washable",
+        "Timeless design"
+    ]
+},
+
+{
+    id: 6,
+    name: 'Áo khoác nam ESEA thiết kế thuê Mỹ áo khoác bóng chày. ',
+    description: '',
+    price: 350.000,
+    originalPrice: 59.99,
+    sold: 596,
+    rating: 4.5,
+    reviews: 48,
+    imageUrl: require('../image/mau6.jpg'),
+    colors: ["White", "Black"],
+    sizes: [ "M", "L"],
+    brand: "DenimBrand",
+    features: [
+        "Comfort stretch denim",
+        "Classic 5-pocket styling",
+        "Machine washable",
+        "Timeless design"
+    ]
+},
+{
+    id: 7,
+    name: 'Đầm thiết kế công chúa  ',
+    description: '',
+    price: 750.000,
+    originalPrice: 59.99,
+    sold: 25,
+    rating: 4.2,
+    reviews: 45,
+    imageUrl: require('../image/mau18.jpg'),
+    colors: ["Pink"],
+    sizes: ["S", "M", "L"],
+    brand: "DenimBrand",
+    features: [
+        "Comfort stretch denim",
+        "Classic 5-pocket styling",
+        "Machine washable",
+        "Timeless design"
+    ]
+},
+{
+    id: 7,
+    name: 'Áo thun form rộng tay lỡ- Unisex ',
+    description: '',
+    price: 220.500,
+    originalPrice: 59.99,
+    sold: 938,
+    rating: 4.3,
+    reviews: 50,
+    imageUrl: require('../image/mau19.jpg'),
+    colors: ["Blue", "Black"],
+    sizes: ["S", "M", "L"],
+    brand: "DenimBrand",
+    features: [
+        "Comfort stretch denim",
+        "Classic 5-pocket styling",
+        "Machine washable",
+        "Timeless design"
+    ]
+},
+{
+    id: 8,
+    name: 'Váy Yếm Xanh Hoàng Gia Phối Váy Xếp Ly Kem',
+    description: '',
+    price: 533.510,
+    originalPrice: 59.99,
+    sold: 938,
+    rating: 5.0,
+    reviews: 79,
+    imageUrl: require('../image/mau17.jpg'),
+    colors: ["Dark Blue"],
+    sizes: ["S", "M", "L"],
+    brand: "DenimBrand",
+    features: [
+        "Comfort stretch denim",
+        "Classic 5-pocket styling",
+        "Machine washable",
+        "Timeless design"
+    ]
+},
   // Add more products as needed
 ];
 
